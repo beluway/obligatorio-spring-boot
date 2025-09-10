7edf834a83974d3f6bef93c1ec2a0c1ab6e72fc4
zimbiosys <mimosaurio09@gmail.com>
2025-09-10 10:07:19 -0300

    .

--- src/main/Controladores/ControladorOfertas.java
+++ src/main/Controladores/ControladorOfertas.java
@@ -1,6 +1,9 @@
 import org.springframework.stereotype.Controller;
-
-
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.bios.edu.uy.obligatorio2025.*;
 @Controller
 @RequestMapping("/ofertas")
 public class ControladorOfertas {
