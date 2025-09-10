689cb52f602f3176c40dffaed22f00cd35a0e74b
zimbiosys <mimosaurio09@gmail.com>
2025-09-10 10:40:53 -0300

    .

--- src/main/Controladores/ControladorPostulaciones.java
+++ src/main/Controladores/ControladorPostulaciones.java
@@ -5,8 +5,44 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.bios.edu.uy.obligatorio2025.*;
 
+
 @Controller
 @RequestMapping("/postulaciones")
 public class ControladorPostulaciones {
-    
+   
+    @GetMapping("/crear")
+    public String clienteCrear(@ModelAttribute postulaciones postulacion)
+    {    
+        return "postulaciones/crear";        
+    }
+
+
+    @PostMapping("/crear")
+    public String clienteCrear (@ModelAttribute @Validated postulaciones postulacion, Model modelo, BindingResult resultado) 
+    {           
+        return "postulaciones/crear";
+    }
+
+
+     @GetMapping("/eliminar")
+    public String clienteCrear(@ModelAttribute postulaciones postulacion)
+    {    
+        return "postulaciones/eliminar";        
+    }
+
+
+    @PostMapping("/eliminar")
+    public String clienteCrear (@ModelAttribute @Validated postulaciones postulacion, Model modelo, BindingResult resultado) 
+    {           
+        return "postulaciones/eliminar";
+    }
+
+
+     @GetMapping("/ver")
+    public String clienteCrear(@ModelAttribute postulaciones postulacion)
+    {    
+        return "postulaciones/ver";        
+    }
+
+
 }
