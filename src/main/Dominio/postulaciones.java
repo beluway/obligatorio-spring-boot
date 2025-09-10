3d0e1922b084db853baf06cf820fa64900db7dec
MatiasScarone <matiasscarone@gmail.com>
2025-09-09 23:28:45 -0300

    esqueleto

---
+++ src/main/Dominio/postulaciones.java
@@ -0,0 +1,14 @@
+import com.bios.edu.uy.obligatorio2025.*;
+
+import io.micrometer.common.lang.NonNull;
+
+public class postulaciones {
+    
+
+    @NonNull
+    private ArrayList<ofertas> listaOfertas;
+
+    @NonNull
+    private postulantes p;
+
+}
