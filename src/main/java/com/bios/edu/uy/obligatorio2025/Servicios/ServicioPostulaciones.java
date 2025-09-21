aca6352285d55db5865ff93b1b4b379cc7809409
zimbiosys <mimosaurio09@gmail.com>
2025-09-21 01:14:47 -0300

    .

--- src/main/java/com/bios/edu/uy/obligatorio2025/Servicios/ServicioPostulaciones.java
+++ src/main/java/com/bios/edu/uy/obligatorio2025/Servicios/ServicioPostulaciones.java
@@ -1,8 +1,9 @@
 package com.bios.edu.uy.obligatorio2025.Servicios;
+
 import org.springframework.stereotype.Service;
 
 
 @Service
-public class ServicioOfertas implements IServicioOfertas {
+public class ServicioPostulaciones implements IServicioPostulaciones{
     
 }
