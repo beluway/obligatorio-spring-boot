8f66e20099475851e7ee2a1c818c56b3d7c3c72b
zimbiosys <mimosaurio09@gmail.com>
2025-09-22 20:10:15 -0300

    .

--- src/main/java/com/bios/edu/uy/obligatorio2025/Controladores/ControladorHome.java
+++ src/main/java/com/bios/edu/uy/obligatorio2025/Controladores/ControladorHome.java
@@ -1,22 +1,15 @@
 package com.bios.edu.uy.obligatorio2025.Controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-
 import org.springframework.web.bind.annotation.PostMapping;
-
 import com.bios.edu.uy.obligatorio2025.Dominio.*;
-
-
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
