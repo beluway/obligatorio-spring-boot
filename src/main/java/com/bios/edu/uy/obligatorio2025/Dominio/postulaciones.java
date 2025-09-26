54c025525748c0410e4a39f121269d4590120672
zimbiosys <mimosaurio09@gmail.com>
2025-09-26 10:18:13 -0300

    .

--- src/main/java/com/bios/edu/uy/obligatorio2025/Dominio/postulaciones.java
+++ src/main/java/com/bios/edu/uy/obligatorio2025/Dominio/postulaciones.java
@@ -1,16 +1,20 @@
 package com.bios.edu.uy.obligatorio2025.Dominio;
 
 
+import java.io.Serializable;
 import java.util.Date;
 
 import jakarta.persistence.Column;
-
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
@@ -21,28 +25,33 @@ import jakarta.validation.constraints.PastOrPresent;
 @Table(name = "postulaciones")
 public class postulaciones {
     
-    @Id
+   /*  @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer id; */
+
+    //CLAVE COMPUESTA (OFERTA + POSTULANTE)
+    @EmbeddedId
+    private PostulacionId id;
 
     @NotNull
     @PastOrPresent
     @Column(name = "fechaPostulacion")
     private Date fechaPostulacion; 
 
+    //LA COLUMNA OFERTA ES UNA UNION (JOIN) CON LA TABLA OFERTA Y ADEMAS FORMA LA PK COMPUESTA    
     @NotNull
+    @MapsId("idOferta")
     @OneToOne //UNA POSTULACIÓN SOLO TIENE UNA OFERTA
     @JoinColumn(name = "oferta", nullable = false)
     private ofertas oferta;
 
+    //LA COLUMNA POSTULANTE ES UNA UNION (JOIN) CON LA TABLA POSTULANTE Y ADEMAS FORMA LA PK COMPUESTA    
     @NotNull
+    @MapsId("usuarioPostulante")
     @OneToOne //UNA POSTULACIÓN SOLO TIENE UN POSTULANTE
     @JoinColumn (name="postulante",nullable = false)
     private postulantes postulante;
 
-    //CLAVES COMPUESTAS
-  /*   @EmbeddedId
-    private ClavesCompuestas foreignKeyUsu; */
 
     public Date getFechaPostulacion() {
         return fechaPostulacion;
@@ -68,17 +77,10 @@ public class postulaciones {
         this.postulante = postulante;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
+  
     public postulaciones(Integer id, @NotNull @PastOrPresent Date fechaPostulacion, @NotNull ofertas oferta,
             @NotNull postulantes postulante) {
-        this.id = id;
+        
         this.fechaPostulacion = fechaPostulacion;
         this.oferta = oferta;
         this.postulante = postulante;
@@ -86,24 +88,20 @@ public class postulaciones {
 
     @Override
     public String toString() {
-        return "postulaciones [id=" + id + ", fechaPostulacion=" + fechaPostulacion + ", oferta=" + oferta
+        return "postulaciones [fechaPostulacion=" + fechaPostulacion + ", oferta=" + oferta
                 + ", postulante=" + postulante + "]";
     }
 
    
-   
- 
- /*   @Embeddable
-   public class ClavesCompuestas implements Serializable{
-
-    @Column(name = "usuario", nullable = false)
-    private String usuario;   
     
-    @Column(name = "id", nullable = false)
-    private Integer id;
+   @Embeddable
+   public class PostulacionId implements Serializable{
+
+    private String usuarioPostulante;  
+    private Integer idOferta;
     
    } 
-    */
+    
  
     
 
